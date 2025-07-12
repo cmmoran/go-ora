52e3d8dc8c4884de15bc0c0da1f355d4bdff9826
Chris Moran <chris@chrismoran.com>
2025-07-12 07:35:41 -0400

    feat: add multi-consumer support for DBMS AQ
    - Introduced multi-consumer functionality in the AQ structure.
    - Added `Subscribe`, `Unsubscribe`, and `DequeueForSubscriber` methods.
    - Enhanced `NewAQ` and `Create` to handle multi-consumer queues.
    - Updated example to demonstrate single and multi-consumer scenarios.

--- dbms/aq.go
+++ dbms/aq.go
@@ -4,23 +4,32 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"errors"
+	"sync/atomic"
 
 	go_ora "github.com/sijms/go-ora/v2"
 )
 
+var ErrQueueNotMultiConsumer = errors.New("queue is not multi-consumer")
+
 type AQ struct {
 	conn          *sql.DB
-	Name          string `db:"QUEUE_NAME"`
-	TableName     string `db:"TB_NAME"`
-	TypeName      string `db:"TYPE_NAME"`
+	multiConsumer atomic.Bool
+	Name          string        `db:"QUEUE_NAME"`
+	TableName     string        `db:"TB_NAME"`
+	TypeName      string        `db:"TYPE_NAME"`
+	MultiConsumer go_ora.PLBool `db:"MULTI_CONSUMER"`
+	MaxRetry      int64         `db:"MAX_RETRY"`
+	RetryDelay    int64         `db:"RETRY_DELAY"`
+	RetentionTime int64         `db:"RETENTION_TIME"`
+	Comment       string        `db:"COMMENT"`
 	Owner         string
-	MaxRetry      int64  `db:"MAX_RETRY"`
-	RetryDelay    int64  `db:"RETRY_DELAY"`
-	RetentionTime int64  `db:"RETENTION_TIME"`
-	Comment       string `db:"COMMENT"`
 }
 
-func NewAQ(conn *sql.DB, name, typeName string) *AQ {
+// NewAQ creates a new instance of AQ with the provided connection, queue name, and type name. The table name is generated with a "_TB" suffix.
+// To make this QA multi-consumer, set AQ.MultiConsumer on the resulting instance.
+// Doing so will enable the AQ.Subscribe, AQ.Unsubscribe, AQ.DequeueForSubscriber functions.
+func NewAQ(conn *sql.DB, name, typeName string, multiConsumer ...bool) *AQ {
+	mc := len(multiConsumer) > 0 && multiConsumer[0]
 	output := &AQ{
 		conn:          conn,
 		Name:          name,
@@ -28,6 +37,7 @@ func NewAQ(conn *sql.DB, name, typeName string) *AQ {
 		TypeName:      typeName,
 		RetentionTime: -1,
 		Comment:       name,
+		MultiConsumer: go_ora.PLBool(mc),
 	}
 	return output
 }
@@ -51,7 +61,11 @@ func (aq *AQ) Create() error {
 		return err
 	}
 	sqlText := `BEGIN
-	DBMS_AQADM.CREATE_QUEUE_TABLE (:TB_NAME, :TYPE_NAME);
+	DBMS_AQADM.CREATE_QUEUE_TABLE (
+		queue_table => :TB_NAME,
+		queue_payload_type => :TYPE_NAME,
+		multiple_consumers => :MULTI_CONSUMER
+	);
 
 	DBMS_AQADM.CREATE_QUEUE (
 		:QUEUE_NAME, :TB_NAME, DBMS_AQADM.NORMAL_QUEUE, 
@@ -59,6 +73,9 @@ func (aq *AQ) Create() error {
 		FALSE, :COMMENT);
 END;`
 	_, err = aq.conn.Exec(sqlText, aq)
+	if err == nil {
+		aq.multiConsumer.Store(bool(aq.MultiConsumer))
+	}
 	return err
 }
 
@@ -76,7 +93,7 @@ END;`
 	return err
 }
 
-// enable both enqueue and dequeue
+// Start enable both enqueue and dequeue
 func (aq *AQ) Start(enqueue, dequeue bool) error {
 	err := aq.validate()
 	if err != nil {
@@ -90,7 +107,7 @@ dbms_aqadm.start_queue (queue_name => :QUEUE_NAME,
 	return err
 }
 
-// disable both enqueue and dequeue
+// Stop disable both enqueue and dequeue
 func (aq *AQ) Stop(enqueue, dequeue bool) error {
 	err := aq.validate()
 	if err != nil {
@@ -102,6 +119,98 @@ dbms_aqadm.stop_queue(queue_name => :QUEUE_NAME,
                        dequeue => :DEQUEUE); 
  END;`, aq.Name, go_ora.PLBool(enqueue), go_ora.PLBool(dequeue))
 	return err
+}
+
+// Subscribe adds a new subscriber (consumer) on a multi-consumer queue.
+// Your queue must have been created with multiple_consumers => TRUE.
+func (aq *AQ) Subscribe(subscriberName string) error {
+	if err := aq.validate(); err != nil {
+		return err
+	}
+	if !aq.multiConsumer.Load() {
+		return errors.New("queue is not multi-consumer")
+	}
+
+	const plsql = `
+BEGIN
+  DBMS_AQADM.ADD_SUBSCRIBER(
+    queue_name      => :QUEUE_NAME,
+    subscriber      => SYS.AQ$_AGENT(name => :SUBSCRIBER, address => NULL, protocol => NULL),
+    rule            => NULL,
+    transformation  => NULL,
+    queue_to_queue  => FALSE,
+    delivery_mode   => DBMS_AQADM.PERSISTENT
+  );
+  COMMIT;
+END;`
+
+	_, err := aq.conn.Exec(
+		plsql,
+		sql.Named("QUEUE_NAME", aq.Name),
+		sql.Named("SUBSCRIBER", subscriberName),
+	)
+	return err
+}
+
+// Unsubscribe unregisters a subscriber from the queue.
+func (aq *AQ) Unsubscribe(subscriberName string) error {
+	if err := aq.validate(); err != nil {
+		return err
+	}
+	if !aq.multiConsumer.Load() {
+		return errors.New("queue is not multi-consumer")
+	}
+	const plsql = `
+	BEGIN
+	DBMS_AQADM.REMOVE_SUBSCRIBER(
+		queue_name => :QUEUE_NAME,
+		subscriber => SYS.AQ$_AGENT(:SUBSCRIBER, NULL, NULL)
+);
+	COMMIT;
+	END;`
+	_, err := aq.conn.Exec(
+		plsql,
+		sql.Named("QUEUE_NAME", aq.Name),
+		sql.Named("SUBSCRIBER", subscriberName),
+	)
+	return err
+}
+
+// DequeueForSubscriber pulls the next message for a given subscriber.
+// It sets deque_options.CONSUMER_NAME so each subscriber sees its own copy.
+func (aq *AQ) DequeueForSubscriber(subscriberName string, message driver.Value, messageSize int) ([]byte, error) {
+	if err := aq.validate(); err != nil {
+		return nil, err
+	}
+	if !aq.multiConsumer.Load() {
+		return nil, ErrQueueNotMultiConsumer
+	}
+
+	const plsql = `
+DECLARE
+  dequeue_options    DBMS_AQ.DEQUEUE_OPTIONS_T;
+  message_properties DBMS_AQ.MESSAGE_PROPERTIES_T;
+BEGIN
+  dequeue_options.VISIBILITY    := DBMS_AQ.IMMEDIATE;
+  dequeue_options.CONSUMER_NAME := :SUBSCRIBER;
+  DBMS_AQ.DEQUEUE(
+    queue_name         => :QUEUE_NAME,
+    dequeue_options    => dequeue_options,
+    message_properties => message_properties,
+    payload            => :MSG,
+    msgid              => :MSG_ID
+  );
+END;`
+
+	var msgID []byte
+	_, err := aq.conn.Exec(
+		plsql,
+		sql.Named("QUEUE_NAME", aq.Name),
+		sql.Named("SUBSCRIBER", subscriberName),
+		sql.Named("MSG", go_ora.Out{Dest: message, Size: messageSize}),
+		sql.Named("MSG_ID", go_ora.Out{Dest: &msgID, Size: 100}),
+	)
+	return msgID, err
 }
 
 func (aq *AQ) Dequeue(message driver.Value, messageSize int) (messageID []byte, err error) {
@@ -121,9 +230,12 @@ BEGIN
 		payload => :MSG,
 		msgid => :MSG_ID);
 END;`
-	_, err = aq.conn.Exec(sqlText, sql.Named("QUEUE_NAME", aq.Name),
+	_, err = aq.conn.Exec(
+		sqlText,
+		sql.Named("QUEUE_NAME", aq.Name),
 		sql.Named("MSG", go_ora.Out{Dest: message, Size: messageSize}),
-		sql.Named("MSG_ID", go_ora.Out{Dest: &messageID, Size: 100}))
+		sql.Named("MSG_ID", go_ora.Out{Dest: &messageID, Size: 100}),
+	)
 	return
 }
 
--- examples/dbms_aq/main.go
+++ examples/dbms_aq/main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/sijms/go-ora/v2"
 	go_ora "github.com/sijms/go-ora/v2"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -43,6 +44,13 @@ func dropUDT(conn *sql.DB) error {
 }
 
 func main() {
+	fmt.Println("DBMS AQ example - single consumer")
+	singleConsumer()
+	fmt.Println("DBMS AQ example - multiple consumer")
+	multipleConsumer()
+}
+
+func singleConsumer() {
 	conn, err := sql.Open("oracle", os.Getenv("DSN"))
 	if err != nil {
 		fmt.Println("can't connect: ", err)
@@ -125,4 +133,109 @@ func main() {
 	fmt.Println("Finish dequeue: ", time.Now().Sub(t))
 	fmt.Println("dequeue message id: ", messageID)
 	fmt.Println("message: ", test)
+}
+
+func multipleConsumer() {
+	conn, err := sql.Open("oracle", os.Getenv("DSN"))
+	if err != nil {
+		fmt.Println("can't connect: ", err)
+		return
+	}
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			fmt.Println("can't close: ", err)
+		}
+	}()
+	err = createUDT(conn)
+	if err != nil {
+		fmt.Println("can't create UDT: ", err)
+		return
+	}
+	defer func() {
+		err = dropUDT(conn)
+		if err != nil {
+			fmt.Println("can't drop UDT: ", err)
+		}
+	}()
+	t := time.Now()
+	err = go_ora.RegisterType(conn, "TEST_TYPE1", "", test1{})
+	if err != nil {
+		fmt.Println("can't register type: ", err)
+		return
+	}
+	fmt.Println("Finish register type: ", time.Now().Sub(t))
+	aq := dbms.NewAQ(conn, "GO_ORA_QU", "TEST_TYPE1", true)
+	t = time.Now()
+	err = aq.Create()
+	if err != nil {
+		fmt.Println("can't create queue: ", err)
+		return
+	}
+	fmt.Println("Finish create queue: ", time.Now().Sub(t))
+	defer func() {
+		t = time.Now()
+		err = aq.Drop()
+		if err != nil {
+			fmt.Println("can't drop queue: ", err)
+		}
+		fmt.Println("Finish drop queue: ", time.Now().Sub(t))
+	}()
+	t = time.Now()
+	err = aq.Start(true, true)
+	if err != nil {
+		fmt.Println("can't enable queue: ", err)
+		return
+	}
+	fmt.Println("Finish start queue: ", time.Now().Sub(t))
+	defer func() {
+		t = time.Now()
+		err = aq.Stop(true, true)
+		if err != nil {
+			fmt.Println("can't stop queue: ", err)
+		}
+		fmt.Println("Finish stop queue: ", time.Now().Sub(t))
+	}()
+	t = time.Now()
+	for i := 0; i < 10; i++ {
+		if err = aq.Subscribe(fmt.Sprintf("SUB_%d", i)); err != nil {
+			fmt.Println("can't subscribe: ", err)
+			return
+		}
+	}
+	go func() {
+		time.Sleep(3 * time.Second)
+		messageID, ierr := aq.Enqueue(test1{
+			Id:   11,
+			Name: "TEST",
+			Data: "DATA",
+		})
+		if ierr != nil {
+			fmt.Println("can't enqueue: ", ierr)
+			return
+		}
+		fmt.Println("Finish  enqueue: ", time.Now().Sub(t))
+		fmt.Println("enqueue message id: ", messageID)
+	}()
+	wg := &sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(iwg *sync.WaitGroup, subscriberId int) {
+			var test test1
+			t = time.Now()
+			fmt.Println("waiting for enqueued message...")
+			messageID, ierr := aq.DequeueForSubscriber(fmt.Sprintf("SUB_%d", subscriberId), &test, 100)
+			if ierr != nil {
+				fmt.Println("can't dequeue: ", ierr)
+				return
+			}
+			fmt.Println("Finish dequeue: ", time.Now().Sub(t))
+			fmt.Println("dequeue message id: ", messageID)
+			fmt.Println(fmt.Sprintf("SUB_%d message: ", subscriberId), test)
+			iwg.Done()
+		}(wg, i)
+	}
+
+	wg.Wait()
+	fmt.Println("Finished all dequeue workers: ", time.Now().Sub(t))
 }
